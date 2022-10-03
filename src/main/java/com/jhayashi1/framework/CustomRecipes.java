d62699ce791561f6290ddaaf653714b1f38053de
jhayashi1 <jhayashi@iastate.edu>
2022-10-02 22:31:52 -0500

    syntax error fix

--- src/main/java/com/jhayashi1/framework/CustomRecipes.java
+++ src/main/java/com/jhayashi1/framework/CustomRecipes.java
@@ -2,11 +2,13 @@ package com.jhayashi1.framework;
 
 import javax.xml.stream.events.Namespace;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.plugin.Plugin;
 
 import com.jhayashi1.Main;
 
@@ -20,21 +22,20 @@ public class CustomRecipes {
     }
 
     private void customArrow() {
+        NamespacedKey key = new NamespacedKey(plugin, "arrow");
         ItemStack arrow = new ItemStack(Material.ARROW, 5);
-        ShapedRecipe arrowRecipe = new ShapedRecipe(new NamespacedKey(plugin, NamespacedKey.MINECRAFT), arrow);
-        arrowRecipe.shape(" ^ ", " | ", "   ");
-
-        arrowRecipe.setIngredient('^', Material.FLINT);
-        arrowRecipe.setIngredient('|', Material.STICK);
-
-        plugin.getServer().addRecipe(arrowRecipe);
+        ShapelessRecipe arrowRecipe = new ShapelessRecipe(key, arrow);
+        arrowRecipe.addIngredient(Material.FLINT);
+        arrowRecipe.addIngredient(Material.STRING);
+        Bukkit.addRecipe(arrowRecipe);
     }
 
     private void customString() {
+        NamespacedKey key = new NamespacedKey(plugin, "string");
         ItemStack string = new ItemStack(Material.STRING, 9);
-        ShapelessRecipe stringRecipe = new ShapelessRecipe(new NamespacedKey(plugin, NamespacedKey.MINECRAFT), string);
+        ShapelessRecipe stringRecipe = new ShapelessRecipe(key, string);
         stringRecipe.addIngredient(Material.WHITE_WOOL);
 
-        plugin.getServer().addRecipe(stringRecipe);
+        Bukkit.addRecipe(stringRecipe);
     }
 }
